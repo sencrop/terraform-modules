d456bb06024e5b027bc41bf2caa8698a67feb1c7
Mathieu DESPRIEE <1590580+mdespriee@users.noreply.github.com>
2021-02-23 19:41:18 +0100

    output alb zone infos

--- ecs-fargate-service/outputs.tf
+++ ecs-fargate-service/outputs.tf
@@ -2,3 +2,10 @@ output "url" {
   value = "https://${var.public_lb_dns_name}.${trim(var.public_lb_dns_zone, ".")}"
 }
 
+output "alb_zone_id" {
+  value = (var.enable_public_lb ? aws_alb.lb[0].zone_id : "") 
+}
+
+output "alb_dns_name" {
+  value = (var.enable_public_lb ? aws_alb.lb[0].dns_name : "")
+}
