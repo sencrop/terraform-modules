adb68cc0c8eb200021b1e3b736fa46e298ba4255
Mathieu DESPRIEE <1590580+mdespriee@users.noreply.github.com>
2022-01-21 12:41:08 +0100

    give a name to task role

--- ecs-fargate-service/service.tf
+++ ecs-fargate-service/service.tf
@@ -142,21 +142,22 @@ locals {
 }
 
 resource "aws_iam_role" "task_role" {
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ecs-tasks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+  name = var.service_name
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ecs-tasks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = var.tags
 }
 
 resource "aws_iam_role_policy_attachment" "custom_policy" {
