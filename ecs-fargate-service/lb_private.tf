65d06ceebf139683c28a55141fd69c9b3ef196b3
Mathieu DESPRIEE <1590580+mdespriee@users.noreply.github.com>
2023-03-29 15:09:17 +0200

    feat(lb): change lb strategies

--- ecs-fargate-service/lb_private.tf
+++ ecs-fargate-service/lb_private.tf
@@ -101,6 +101,10 @@ resource "aws_alb_target_group" "lb_priv" {
   vpc_id      = var.vpc_id
   target_type = "ip"
 
+  # avoid oveerloading a loaded host
+  load_balancing_algorithm_type = "least_outstanding_requests"
+  slow_start = 30  # seconds before sending full share of requests to a new target
+
   health_check {
     path                = var.healthcheck_path
     interval            = var.healthcheck_interval
