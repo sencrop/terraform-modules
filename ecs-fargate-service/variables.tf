ec46b8b8c7cc8dbdfd8c4cd09632f3f83a2dabe2
Jerome Dassonville <jerome.dassonville@sencrop.com>
2024-03-29 14:26:25 +0100

    doc(ecs-fargate-service): make variables.tf easier to navigate

--- ecs-fargate-service/variables.tf
+++ ecs-fargate-service/variables.tf
@@ -1,3 +1,6 @@
+/**************************************************
+ * Fargate task configuration
+ * ***********************************************/
 variable "cpu" {
   description = <<EOS
 The hard limit of CPU units to present for the task.
@@ -43,9 +46,6 @@ variable "task_role_policies_arn" {
   default = []
   type    = list(any)
 }
-variable "lb_subnets" {
-  description = "list of subnet IDs for the public LB"
-}
 variable "task_subnets" {}
 variable "ecs_cluster_id" {}
 variable "additional_security_groups" {
@@ -76,71 +76,44 @@ variable "availability_sidecar_enable" {
 variable "availability_sidecar_image" {
   default = "812957082909.dkr.ecr.eu-central-1.amazonaws.com/observability-tools:8c06fd0"
 }
-variable "datadog_agent_image_tag" {
-  default = "812957082909.dkr.ecr.eu-central-1.amazonaws.com/public-ecr/datadog/agent:latest"
-  type    = string
-}
-variable "datadog_agent_memory" {
-  default = 256
-  type = number
-  description = "memory allocated to the Datadog agent container"
-}
-variable "collect_datadog_agent_logs" {
-  default = false
-  type    = bool
-}
-
-variable "enable_datadog_runtime_metrics" {
-  default = false
-  type    = bool
-}
-variable "healthcheck_path" {
-  default = ""
-}
-variable "healthcheck_port" {
-  default = 0
-}
-variable "healthcheck_interval" {
-  default = 5
-}
 variable "healthcheck_grace_period" {
   default     = 30
   type        = number
   description = "Failed health check won't be accounted to determine the health status of the task during the grace period"
 }
-variable "healthcheck_timeout" {
-  default     = 2
-  type        = number
-  description = "timeout range must be between 2 and 120 seconds"
-}
-variable "healthcheck_matcher" {
-  default = "200-399"
-}
-variable "healthcheck_unhealthy_threshold" {
-  default = 5
-}
-variable "lb_certificate_arn" {
-  default = ""
-}
 variable "waf_acl_arn" {
   default = ""
 }
-variable "public_lb_dns_zone" {
-  default     = ""
-  description = "foo.bar."
+variable "tags" {
+  default = {}
 }
-variable "public_lb_dns_name" {
-  default = ""
+variable "docker_ulimits" {
+  description = "see https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_Ulimit.html"
+  default     = []
 }
-variable "public_lb_idle_timeout" {
+variable "stop_timeout" {
   type        = number
-  default     = 60
-  description = "How long in seconds the load balancer will keep an idle connection open with the backend"
+  default     = 5
+  description = "How long in seconds the orchestrator will wait to send a SIGKILL after a SIGTERM, max 120 seconds"
 }
-variable "enable_public_lb" {
-  default     = true
-  description = "Add a public loadbalancer in front of the service"
+variable "task_public_ip" {
+  type        = bool
+  default     = false
+  description = "Assign a public IP directly to the task, the task must be deployed on a public subnet"
 }
+
+variable "enable_execute_command" {
+  type        = bool
+  default     = false
+  description = "Allow an operator to exec inisde containers using aws ecs execute-command"
+}
+variable "task_definition_only" {
+  default     = false
+  description = "When set to true only the task definition will be published"
+}
+/**************************************************
+ * Service discovery
+ * ***********************************************/
 variable "enable_local_discovery" {
   default = false
   # TODO validation: if true, it requires to declare other vars as well
@@ -151,6 +124,10 @@ variable "local_discovery_service_name" {
 variable "discovery_namespace_id" {
   default = ""
 }
+
+/**************************************************
+ * Autoscaling configuration
+ * ***********************************************/
 variable "enable_autoscale" {
   default = false
 }
@@ -173,87 +150,47 @@ variable "autoscale_scale_out_cooldown" {
   type        = number
   description = "Amount of time, in seconds, after a scale out activity completes before another scale out activity can start"
 }
-variable "tags" {
-  default = {}
-}
-variable "dd_tags" {
-  default     = {}
-  type        = map(string)
-  description = "tags added to the DD_TAGS environment variable of the main task"
-}
+
+/**************************************************
+ *  Load balancer configuration
+ * ***********************************************/
 variable "deregistration_delay" {
   type        = number
   description = "How long in second a load balancer will keep a target in DRAINING state before removing it from the targets. Most of the time this should be aligned with the idle timeout."
   default     = 60
 }
-variable "docker_ulimits" {
-  description = "see https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_Ulimit.html"
-  default     = []
+variable "lb_algorithm_type" {
+  default     = "round_robin"
+  description = "Possible values 'round_robin' or 'least_outstanding_requests'. Applies to any type of LB (public or private)."
 }
-variable "logs" {
-  type        = string
-  default     = "cloudwatch"
-  description = "Should be \"cloudwatch\" or \"datadog\"."
+# Public load balancer
+variable "enable_public_lb" {
+  default     = true
+  description = "Add a public loadbalancer in front of the service"
 }
-variable "datadog_api_key" {
-  default   = ""
-  sensitive = true
-}
-variable "logs_json" {
-  default     = false
-  description = "Whether logs should be parsed as json. Works together with datadog logs."
-}
-variable "datadog_log_source" {
-  default     = "default"
-  description = "maps to the datadog ingestion parser"
-}
-variable "enable_datadog_agent" {
-  default     = false
-  description = "To enable a side-car with datadog agent to collect Fargate tasks metrics"
-}
-variable "enable_datadog_agent_apm" {
-  default     = false
-  description = "To enable APM data collection in datadog agent. See https://docs.datadoghq.com/integrations/ecs_fargate/?tab=fluentbitandfirelens#trace-collection"
-}
-variable "enable_datadog_non_local_apm" {
-  default     = false
-  description = "To enable APM data collection in datadog agent with APM features."
-}
-variable "enable_datadog_dogstatsd_non_local_traffic" {
-  default     = false
-  description = "To enable DogStatsD data collection in datadog agent"
-}
-variable "datadog_apm_ignore_ressources" {
-  default     = []
-  description = "List of ressources (span names) to ignore in the APM. See https://docs.datadoghq.com/tracing/guide/ignoring_apm_resources/"
-}
-variable "enable_datadog_logs_injection" {
-  default     = false
-  description = "To inject trace IDs, span IDs, env, service, and version in the logs. See https://docs.datadoghq.com/tracing/connect_logs_and_traces/"
-}
-variable "datadog_receiver_otlp_http_endpoint" {
-  default     = "localhost:4318"
-  description = "HTTP endpoint of the datadog agent used to receive traces in OpenTelemetry format"
-}
-variable "enable_datadog_src_code_integration" {
-  type        = bool
-  default     = false
-  description = "Enable the (APM) source code integration, commit_sha and repository_url should be defined as well"
-}
-variable "commit_sha" {
-  type        = string
-  default     = ""
-  description = "git commit sha of the configured service version"
-}
-variable "repository_url" {
-  type        = string
-  default     = ""
-  description = "http url of the git repository"
+variable "lb_subnets" {
+  description = "list of subnet IDs for the public LB"
 }
 variable "public_lb_access_logs_bucket" {
   description = "Where to put public LB access logs."
   default     = ""
 }
+variable "public_lb_dns_zone" {
+  default     = ""
+  description = "foo.bar."
+}
+variable "public_lb_dns_name" {
+  default = ""
+}
+variable "public_lb_idle_timeout" {
+  type        = number
+  default     = 60
+  description = "How long in seconds the load balancer will keep an idle connection open with the backend"
+}
+variable "lb_certificate_arn" {
+  default = ""
+}
+# Private load balancer
 variable "enable_private_lb" {
   default = false
 }
@@ -281,34 +218,124 @@ variable "lb_private_additional_security_groups" {
   description = "additional security groups for private LB"
   default     = []
 }
-variable "task_definition_only" {
-  default = false
-}
-variable "datadog_mapper" {
-  default     = ""
-  description = "Where to put some inline mappings between metrics emitted by some software and Datadog. Used only for Airflow as of now."
-}
-
-variable "lb_algorithm_type" {
-  default     = "round_robin"
-  description = "Possible values 'round_robin' or 'least_outstanding_requests'. Applies to any type of LB (public or private)."
-}
-variable "stop_timeout" {
+# Healthcheck
+variable "healthcheck_timeout" {
+  default     = 2
   type        = number
-  default     = 5
-  description = "How long in seconds the orchestrator will wait to send a SIGKILL after a SIGTERM, max 120 seconds"
+  description = "timeout range must be between 2 and 120 seconds"
 }
-variable "task_public_ip" {
-  type        = bool
-  default     = false
-  description = "Assign a public IP directly to the task, the task must be deployed on a public subnet"
+variable "healthcheck_path" {
+  default = ""
+}
+variable "healthcheck_port" {
+  default = 0
+}
+variable "healthcheck_interval" {
+  default = 5
+}
+variable "healthcheck_matcher" {
+  default = "200-399"
+}
+variable "healthcheck_unhealthy_threshold" {
+  default = 5
+}
+/**************************************************
+ * Logging configuration
+ * ***********************************************/
+variable "logs" {
+  type        = string
+  default     = "cloudwatch"
+  description = "Should be \"cloudwatch\" or \"datadog\"."
 }
 variable "fluent_bit_image_tag" {
   type    = string
   default = "812957082909.dkr.ecr.eu-central-1.amazonaws.com/public-ecr/aws-observability/aws-for-fluent-bit:2.19.0"
 }
-variable "enable_execute_command" {
+variable "logs_json" {
+  default     = false
+  description = "Whether logs should be parsed as json. Works together with datadog logs."
+}
+variable "datadog_log_source" {
+  default     = "default"
+  description = "maps to the datadog ingestion parser"
+}
+
+/**************************************************
+ * Datadog agent configuration
+ * ***********************************************/
+variable "datadog_agent_image_tag" {
+  default = "812957082909.dkr.ecr.eu-central-1.amazonaws.com/public-ecr/datadog/agent:latest"
+  type    = string
+}
+variable "datadog_agent_memory" {
+  default     = 256
+  type        = number
+  description = "memory allocated to the Datadog agent container"
+}
+variable "collect_datadog_agent_logs" {
+  default = false
+  type    = bool
+}
+variable "enable_datadog_runtime_metrics" {
+  default = false
+  type    = bool
+}
+variable "enable_datadog_agent" {
+  default     = false
+  description = "To enable a side-car with datadog agent to collect Fargate tasks metrics"
+}
+variable "datadog_api_key" {
+  default   = ""
+  sensitive = true
+}
+variable "dd_tags" {
+  default     = {}
+  type        = map(string)
+  description = "tags added to the DD_TAGS environment variable of the main task"
+}
+variable "datadog_mapper" {
+  default     = ""
+  description = "Where to put some inline mappings between metrics emitted by some software and Datadog. Used only for Airflow as of now."
+}
+# Metrics
+variable "enable_datadog_dogstatsd_non_local_traffic" {
+  default     = false
+  description = "To enable DogStatsD data collection in datadog agent"
+}
+# APM
+variable "enable_datadog_agent_apm" {
+  default     = false
+  description = "To enable APM data collection in datadog agent. See https://docs.datadoghq.com/integrations/ecs_fargate/?tab=fluentbitandfirelens#trace-collection"
+}
+variable "enable_datadog_non_local_apm" {
+  default     = false
+  description = "To enable APM data collection in datadog agent with APM features."
+}
+variable "datadog_apm_ignore_ressources" {
+  default     = []
+  description = "List of ressources (span names) to ignore in the APM. See https://docs.datadoghq.com/tracing/guide/ignoring_apm_resources/"
+}
+variable "enable_datadog_logs_injection" {
+  default     = false
+  description = "To inject trace IDs, span IDs, env, service, and version in the logs. See https://docs.datadoghq.com/tracing/connect_logs_and_traces/"
+}
+variable "datadog_receiver_otlp_http_endpoint" {
+  default     = "localhost:4318"
+  description = "HTTP endpoint of the datadog agent used to receive traces in OpenTelemetry format"
+}
+# Source code integration
+variable "enable_datadog_src_code_integration" {
   type        = bool
   default     = false
-  description = "Allow an operator to exec inisde containers using aws ecs execute-command"
+  description = "Enable the (APM) source code integration, commit_sha and repository_url should be defined as well"
+}
+variable "commit_sha" {
+  type        = string
+  default     = ""
+  description = "git commit sha of the configured service version"
+}
+variable "repository_url" {
+  type        = string
+  default     = ""
+  description = "http url of the git repository"
 }
